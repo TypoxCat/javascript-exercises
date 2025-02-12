fb22de95b5cc6d337fd71c966926b94db781ddf7
Xeip <177243908+Xeip@users.noreply.github.com>
2025-02-12 19:44:09 +0700

    Done 01-04 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    kata = "";
+    if (num < 0){
+        return "ERROR";
+    }
+    while (num > 0){
+        kata = kata + string;
+        num--;
+    }
+    return kata;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(kata) {
+    // Input string to array and reverse the index
+    const arrayKata = [];
+    const lengthKata = kata.length;
+    let letter = 0;
+    for (i = lengthKata-1; i >= 0; i--){
+        arrayKata[i] = kata.substr(letter, 1);
+        letter++;
+    }
+    return arrayKata.join("");
 };
 
 // Do not edit below this line
