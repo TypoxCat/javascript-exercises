49f0e4716801c2f77182c3f51a97c76977abb434
Xeip <177243908+Xeip@users.noreply.github.com>
2025-02-15 20:07:21 +0700

    Done 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(init, end) {
+    let jumlah = 0;
+    // init always smaller than end 
+    if (end < init){
+        let temp = init;
+        init = end;
+        end = temp;
+    }
+    // input MUST integer
+    if (init < 0 || end < 0 || !Number.isInteger(init)|| !Number.isInteger(end)){
+        return "ERROR";
+    }
 
+    for(i = init; i <= end; i++){
+        jumlah += i;
+    }
+    return jumlah;
 };
 
 // Do not edit below this line
