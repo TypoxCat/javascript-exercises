feff5a7eeb36223a18ab03469cd75ddb5a9a7efa
Xeip <177243908+Xeip@users.noreply.github.com>
2025-02-15 21:08:25 +0700

    Done 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(awal, ...args) {
+  let total = awal;
+  args.forEach((item) => {
+    total += item;
+  });
+	return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(awal, ...args) {
+	let total = awal;
+  args.forEach((item) => {
+    total -= item;
+  });
+	return total;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (!array.length){
+    return 0;
+  }
+  return array.reduce(function(total, num) {
+    return total + num;
+  });
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (!array.length){
+    return 0;
+  }
+  return array.reduce(function(total, num) {
+    return total * num;
+  });
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let hasil = number;
+  if (number < 1){
+    return 1;
+  }
+
+	for (i = number-1; i > 0; i--){
+    hasil *= i;
+  }
+  return hasil;
 };
 
 // Do not edit below this line
