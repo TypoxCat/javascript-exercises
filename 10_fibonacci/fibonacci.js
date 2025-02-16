38a49f5eae73ac594cc10dc402cf18864e7c4d09
Xeip <177243908+Xeip@users.noreply.github.com>
2025-02-16 14:35:00 +0700

    Done 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0){
+        return "OOPS";
+    }
+    let sum = [0, 1];
+    for (i = 0; i <= num; i++){
+        if (i == num){
+            return sum[i];
+        }
+        sum[i+2] = sum[i] + sum[i+1];
+    }
 };
 
 // Do not edit below this line
