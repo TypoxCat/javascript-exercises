ba98ac73702df197e570ec2721b9b1b30a9f92c7
Xeip <177243908+Xeip@users.noreply.github.com>
2025-02-16 17:48:00 +0700

    Done 11-12 exercises, final

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,68 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let name, age, birth, death, noDeathName;
+    let maxAgeName, maxAge = 0, maxBirth = 0;
+    let noDeathData = false;
 
+    // condition if there is undefined death year
+    array.forEach(element => {
+        if (element.yearOfDeath == undefined){
+            maxBirth = element.yearOfBirth;
+            noDeathData = true;
+            noDeathName = element.name;
+            console.log(maxBirth);
+        }
+    });
+    // main algorithm
+    if (noDeathData){
+        compareBirthYear();
+        if (maxAgeName != noDeathName){
+            compareAges();
+        }
+    } else {
+        compareAges();
+    }
+
+    // functions
+    function compareBirthYear(){
+        array.forEach(element => {
+            defineVariable(element.name, element.yearOfBirth, element.yearOfDeath);
+            console.log(name, birth, death, age);
+            if (birth <= maxBirth){
+                newOldest(name, age, birth);
+            }
+            console.log(maxAgeName);
+        });
+    }
+
+    function compareAges (){
+        array.forEach(element => {
+            defineVariable(element.name, element.yearOfBirth, element.yearOfDeath);
+            // console.log(name, birth, death, age);
+            if (age > maxAge){
+                newOldest(name, age, birth);
+            }
+            // console.log(maxAgeName);
+    });
+    }
+
+    function defineVariable(N, B, D){
+        name = N;
+        birth = B;
+        death = D;
+        age = death-birth;
+    }
+
+    function newOldest(n, a, b){
+        maxAgeName = n;
+        maxAge = a;
+        maxBirth = b;
+    }
+
+    // to output
+    const oldest = {
+        name: maxAgeName
+    }
+    return oldest;
 };
 
 // Do not edit below this line
